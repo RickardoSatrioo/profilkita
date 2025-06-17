b55052ba210beb91f1490ffcaac8510340fbdaaf
Rickardo Satrio <rickardosatrio@gmail.com>
2025-06-17 23:14:09 +0700

    Mengambil gambar dari kamera

--- app/src/main/java/com/rickardosatrioabout/profilkita/ui/theme/screen/MainScreen.kt
+++ app/src/main/java/com/rickardosatrioabout/profilkita/ui/theme/screen/MainScreen.kt
@@ -1,13 +1,14 @@
 package com.rickardosatrioabout.profilkita.ui.theme.screen
 
+import android.content.ContentResolver
 import android.content.Context
 import android.content.res.Configuration
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.os.Build
+import android.provider.MediaStore
 import android.util.Log
-import androidx.credentials.exceptions.ClearCredentialException
-import androidx.credentials.ClearCredentialStateRequest
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -21,9 +22,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -34,6 +38,10 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -46,14 +54,20 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.credentials.ClearCredentialStateRequest
 import androidx.credentials.CredentialManager
 import androidx.credentials.CustomCredential
 import androidx.credentials.GetCredentialRequest
 import androidx.credentials.GetCredentialResponse
+import androidx.credentials.exceptions.ClearCredentialException
 import androidx.credentials.exceptions.GetCredentialException
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import com.canhub.cropper.CropImageContract
+import com.canhub.cropper.CropImageContractOptions
+import com.canhub.cropper.CropImageOptions
+import com.canhub.cropper.CropImageView
 import com.google.android.libraries.identity.googleid.GetGoogleIdOption
 import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
 import com.google.android.libraries.identity.googleid.GoogleIdTokenParsingException
@@ -64,17 +78,20 @@ import com.rickardosatrioabout.profilkita.model.User
 import com.rickardosatrioabout.profilkita.model.UserDataStore
 import com.rickardosatrioabout.profilkita.network.MahasiswaApi
 import com.rickardosatrioabout.profilkita.ui.theme.ProfilkitaTheme
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainScreen(){
+fun MainScreen() {
     val context = LocalContext.current
-    val dataStore = UserDataStore(context)
+    val dataStore = remember { UserDataStore(context) }
     val user by dataStore.userFlow.collectAsState(User())
+    val scope = rememberCoroutineScope()
     var showDialog by remember { mutableStateOf(false) }
+
+    val launcher = rememberLauncherForActivityResult(CropImageContract()) { result ->
+        getCroppedImage(context.contentResolver, result)
+    }
 
     Scaffold(
         topBar = {
@@ -89,9 +106,8 @@ fun MainScreen(){
                 actions = {
                     IconButton(onClick = {
                         if (user.email.isEmpty()) {
-                            CoroutineScope(Dispatchers.IO).launch { signIn(context, dataStore) }
-                        }
-                        else{
+                            scope.launch { signIn(context, dataStore) }
+                        } else {
                             showDialog = true
                         }
                     }) {
@@ -103,6 +119,23 @@ fun MainScreen(){
                     }
                 }
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                val option = CropImageContractOptions(
+                    null, CropImageOptions(
+                        imageSourceIncludeGallery = false,
+                        imageSourceIncludeCamera = true,
+                        fixAspectRatio = true
+                    )
+                )
+                launcher.launch(option)
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(id = R.string.tambah_mahasiswa)
+                )
+            }
         }
     ) { innerPadding ->
         ScreenContent(Modifier.padding(innerPadding))
@@ -110,88 +143,50 @@ fun MainScreen(){
         if (showDialog) {
             ProfilDialog(
                 user = user,
-                onDismissRequest = { showDialog = false }) {
-                CoroutineScope(Dispatchers.IO).launch { signOut(context, dataStore) }
+                onDismissRequest = { showDialog = false }
+            ) {
+                scope.launch { signOut(context, dataStore) }
                 showDialog = false
             }
         }
     }
 }
 
-private suspend fun signIn(context: Context, dataStore: UserDataStore){
-    val googleIdOption: GetGoogleIdOption = GetGoogleIdOption.Builder()
-        .setFilterByAuthorizedAccounts(false)
-        .setServerClientId(BuildConfig.API_KEY)
-        .build()
-
-    val request: GetCredentialRequest = GetCredentialRequest.Builder()
-        .addCredentialOption(googleIdOption)
-        .build()
-
-    try {
-        val credentialManager = CredentialManager.create(context)
-        val result = credentialManager.getCredential(context, request)
-        handleSignIn(result, dataStore)
-    } catch (e: GetCredentialException){
-        Log.e("SIGN-IN", "Error: ${e.errorMessage}")
-    }
-}
-
-private fun handleSignIn(result: GetCredentialResponse, dataStore: UserDataStore) {
-    val credential = result.credential
-    if (credential is CustomCredential &&
-        credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
-        try {
-            val googleIdToken = GoogleIdTokenCredential.createFrom(credential.data)
-            val nama = googleIdToken.displayName ?: ""
-            val email = googleIdToken.id
-            val photoUrl = googleIdToken.profilePictureUri.toString()
-            CoroutineScope(Dispatchers.IO).launch {
-                dataStore.saveData(User(nama, email, photoUrl))
-            }
-        } catch (e: GoogleIdTokenParsingException) {
-            Log.e("SIGN-IN", "Error: ${e.message}")
-        }
-    }
-    else {
-        Log.e("SIGN-IN", "Error: unrecognized custom credential type.")
-    }
-}
-
 @Composable
-fun ScreenContent(modifier: Modifier = Modifier){
+fun ScreenContent(modifier: Modifier = Modifier) {
     val viewModel: MainViewModel = viewModel()
     val data by viewModel.data
-
     val status by viewModel.status.collectAsState()
 
-    when (status){
-            MahasiswaApi.ApiStatus.LOADING ->{
-        Box (
-            modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
-        ) {
-            CircularProgressIndicator()
-        }
-    }
-        MahasiswaApi.ApiStatus.SUCCESS -> {
-            LazyVerticalGrid(
-                modifier = modifier.fillMaxSize().padding(4.dp),
-                columns = GridCells.Fixed(2),
+    when (status) {
+        MahasiswaApi.ApiStatus.LOADING -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
             ) {
-                items(data) {ListItem(mahasiswa = it)}
+                CircularProgressIndicator()
             }
         }
-
+        MahasiswaApi.ApiStatus.SUCCESS -> {
+            LazyVerticalGrid(
+                modifier = modifier
+                    .fillMaxSize()
+                    .padding(4.dp),
+                columns = GridCells.Fixed(2),
+                contentPadding = PaddingValues(bottom = 80.dp)
+            ) {
+                items(data) { ListItem(mahasiswa = it) }
+            }
+        }
         MahasiswaApi.ApiStatus.FAILED -> {
-            Column (
+            Column(
                 modifier = Modifier.fillMaxSize(),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
-            ){
+            ) {
                 Text(text = stringResource(id = R.string.erorr))
                 Button(
-                    onClick = { viewModel.retrieveData()},
+                    onClick = { viewModel.retrieveData() },
                     modifier = Modifier.padding(16.dp),
                     contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
                 ) {
@@ -199,18 +194,6 @@ fun ScreenContent(modifier: Modifier = Modifier){
                 }
             }
         }
-    }
-}
-
-private suspend fun signOut(context: Context, dataStore: UserDataStore) {
-    try {
-        val credentialManager = CredentialManager.create(context)
-        credentialManager.clearCredentialState(
-            ClearCredentialStateRequest()
-        )
-        dataStore.saveData(User())
-    } catch (e: ClearCredentialException) {
-        Log.e("SIGN-IN", "Error: ${e.errorMessage}")
     }
 }
 
@@ -236,12 +219,12 @@ fun ListItem(mahasiswa: Mahasiswa) {
                 .aspectRatio(1f)
         )
 
-        Column (
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .background(Color(red = 0f, green = 0f, blue = 0f, alpha = 0.5f))
+                .background(Color.Black.copy(alpha = 0.5f))
                 .padding(4.dp)
-        ){
+        ) {
             Text(
                 text = mahasiswa.nama,
                 fontWeight = FontWeight.Bold,
@@ -263,6 +246,75 @@ fun ListItem(mahasiswa: Mahasiswa) {
     }
 }
 
+private suspend fun signIn(context: Context, dataStore: UserDataStore) {
+    val googleIdOption: GetGoogleIdOption = GetGoogleIdOption.Builder()
+        .setFilterByAuthorizedAccounts(false)
+        .setServerClientId(BuildConfig.API_KEY)
+        .build()
+
+    val request: GetCredentialRequest = GetCredentialRequest.Builder()
+        .addCredentialOption(googleIdOption)
+        .build()
+
+    try {
+        val credentialManager = CredentialManager.create(context)
+        val result = credentialManager.getCredential(context, request)
+        handleSignIn(result, dataStore)
+    } catch (e: GetCredentialException) {
+        Log.e("SIGN-IN", "Error: ${e.errorMessage}")
+    }
+}
+
+private suspend fun handleSignIn(result: GetCredentialResponse, dataStore: UserDataStore) {
+    val credential = result.credential
+    if (credential is CustomCredential &&
+        credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL
+    ) {
+        try {
+            val googleIdToken = GoogleIdTokenCredential.createFrom(credential.data)
+            val nama = googleIdToken.displayName ?: ""
+            val email = googleIdToken.id
+            val photoUrl = googleIdToken.profilePictureUri.toString()
+            dataStore.saveData(User(nama, email, photoUrl))
+        } catch (e: GoogleIdTokenParsingException) {
+            Log.e("SIGN-IN", "Error: ${e.message}")
+        }
+    } else {
+        Log.e("SIGN-IN", "Error: unrecognized custom credential type.")
+    }
+}
+
+private suspend fun signOut(context: Context, dataStore: UserDataStore) {
+    try {
+        val credentialManager = CredentialManager.create(context)
+        credentialManager.clearCredentialState(
+            ClearCredentialStateRequest()
+        )
+        dataStore.saveData(User())
+    } catch (e: ClearCredentialException) {
+        Log.e("SIGN-IN", "Error: ${e.errorMessage}")
+    }
+}
+
+private fun getCroppedImage(
+    resolver: ContentResolver,
+    result: CropImageView.CropResult
+): Bitmap? {
+    if (!result.isSuccessful) {
+        Log.e("IMAGE", "Error: ${result.error?.message}")
+        return null
+    }
+
+    val uri = result.uriContent ?: return null
+
+    return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
+        @Suppress("DEPRECATION")
+        MediaStore.Images.Media.getBitmap(resolver, uri)
+    } else {
+        val source = ImageDecoder.createSource(resolver, uri)
+        ImageDecoder.decodeBitmap(source)
+    }
+}
 
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
